16477c189d6041f8928481e84c5774464058d895
juloo <juloo.dsi@gmail.com>
2015-09-04 00:07:37 +0200

    Fix app start detection

--- logcat.py
+++ logcat.py
@@ -7,7 +7,7 @@
 #    By: juloo <juloo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/08/04 18:31:55 by juloo             #+#    #+#              #
-#    Updated: 2015/08/04 20:10:14 by juloo            ###   ########.fr        #
+#    Updated: 2015/09/04 00:05:31 by juloo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,23 +15,30 @@ from subprocess import Popen, PIPE
 from sys import argv, stdout, exit
 import re
 
+prefixReg = re.compile('[^:]+\(\s*([0-9]+)\):.*')
+startAppReg = re.compile('^./ActivityManager[^:]+:\s*Start proc.+$')
+
+def search_pid(app_name):
+	pids = []
+	cmd = Popen("adb shell ps".split(), stdout=PIPE)
+	for l in cmd.stdout:
+		if app_name in l:
+			pids.append(int(l.split()[1]))
+	return pids
+
+# main
+
 if len(argv) <= 1:
 	print("\033[31mNot enougth argument\033[39m")
 	print("Usage: %s <app name (package name)>" % argv[0])
 	exit()
-
-prefixReg = re.compile('[^:]+\(\s*([0-9]+)\):.*')
-startAppReg = re.compile('[^:]+: Start proc ([^ ]+)[^:]+:\s*pid\s*=\s*([0-9]+)\s*.*')
 
 app_name = argv[1]
 pids = []
 
 Popen("adb logcat -c".split()).wait()
 
-cmd = Popen("adb shell ps".split(), stdout=PIPE)
-for l in cmd.stdout:
-	if app_name in l:
-		pids.append(int(l.split()[1]))
+pids = search_pid(app_name)
 
 if len(pids) <= 0:
 	stdout.write("\033[91mApp not started. Waiting")
@@ -44,11 +51,10 @@ stdout.flush()
 
 cmd = Popen("adb logcat".split(), stdout=PIPE)
 for l in iter(cmd.stdout.readline, ""):
-	m = startAppReg.match(l)
-	if m != None and app_name in m.group(1):
-		pid = int(m.group(2))
-		if pid not in pids:
-			pids.append(pid)
+	if startAppReg.match(l) != None:
+		new_pids = search_pid(app_name)
+		if new_pids != pids:
+			pids = new_pids
 			stdout.write("\033[32mApplication started. pid:")
 			for p in pids:
 				stdout.write(" %d" % p)
